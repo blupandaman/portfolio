00b362a3917e5ab06dc0a314ad0fad489b0848fa
blupandaman <blupandaman@pm.me>
2024-05-23 09:25:08 -0400

    feat: add framer motion for nav bar animation

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -13,22 +14,21 @@ export function Navbar() {
   const pathname = usePathname();
 
   return (
-    <div className="absolute left-4 top-4 flex space-x-3">
-      {navigation.map((nav) => {
-        if (pathname === nav.href) {
-          return (
-            <p key={nav.label} className="underline underline-offset-4">
-              {nav.label}
-            </p>
-          );
-        }
-
-        return (
-          <Link key={nav.label} href={nav.href}>
+    <div className="absolute left-4 top-4 z-50 flex space-x-4 sm:space-x-3">
+      {navigation.map((nav) => (
+        <div key={nav.label} className="relative flex flex-col">
+          <Link href={nav.href} className="pb-2.5 sm:pb-2">
             {nav.label}
           </Link>
-        );
-      })}
+          {pathname === nav.href && (
+            <motion.span
+              layoutId="underline"
+              className="absolute bottom-0 h-1 w-full rounded-full bg-primary"
+              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+            />
+          )}
+        </div>
+      ))}
     </div>
   );
 }
