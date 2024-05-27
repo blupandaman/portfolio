556d98f22644b2483c0612460281f4f54065b834
blupandaman <blupandaman@pm.me>
2024-05-27 07:30:29 -0400

    chore: remove tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { TooltipProvider } from "@/components/ui/tooltip";
 import Link from "next/link";
 import Image, { type StaticImageData } from "next/image";
 import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 import { Badge } from "@/components/ui/badge";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import {
   Popover,
   PopoverContent,
@@ -107,30 +101,36 @@ function ProjectContent(props: { project: (typeof projects)[0] }) {
 
 export default function HomePage() {
   return (
-    <TooltipProvider delayDuration={50}>
-      <div className="flex flex-1 flex-wrap justify-between gap-4 sm:flex-col sm:justify-normal sm:gap-3">
-        {projects.map((project) => (
-          <>
-            <div className="hidden sm:block">
-              <Tooltip>
-                <TooltipTrigger>{project.label}</TooltipTrigger>
-                <TooltipContent side="right" className="space-y-2 p-2">
-                  <ProjectContent project={project} />
-                </TooltipContent>
-              </Tooltip>
-            </div>
+    <div className="flex flex-1 flex-wrap justify-between gap-4 sm:flex-col sm:justify-normal sm:gap-3">
+      {projects.map((project) => (
+        <>
+          <div className="hidden sm:block">
+            <Popover>
+              <PopoverTrigger>{project.label}</PopoverTrigger>
+              <PopoverContent
+                sideOffset={16}
+                side="right"
+                className="space-y-2 p-2"
+              >
+                <ProjectContent project={project} />
+              </PopoverContent>
+            </Popover>
+          </div>
 
-            <div className="block sm:hidden">
-              <Popover>
-                <PopoverTrigger>{project.label}</PopoverTrigger>
-                <PopoverContent side="bottom" className="space-y-2 p-2">
-                  <ProjectContent project={project} />
-                </PopoverContent>
-              </Popover>
-            </div>
-          </>
-        ))}
-      </div>
-    </TooltipProvider>
+          <div className="block sm:hidden">
+            <Popover>
+              <PopoverTrigger>{project.label}</PopoverTrigger>
+              <PopoverContent
+                sideOffset={16}
+                side="bottom"
+                className="space-y-2 p-2"
+              >
+                <ProjectContent project={project} />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </>
+      ))}
+    </div>
   );
 }
